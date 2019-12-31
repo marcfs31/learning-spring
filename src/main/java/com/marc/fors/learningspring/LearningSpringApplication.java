2e0812d772e275d2e799cb8f4e203ca9e3ddba09
Marc Fors <marcfs31@protonmail.com>
2019-12-31 20:31:52 +0100

    Spring Boot webapp with embedded database.

---
+++ src/main/java/com/marc/fors/learningspring/LearningSpringApplication.java
@@ -0,0 +1,32 @@
+package com.marc.fors.learningspring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marc.fors.learningspring.data.entity.Room;
+import com.marc.fors.learningspring.data.repository.RoomRepository;
+
+@SpringBootApplication
+public class LearningSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringApplication.class, args);
+	}
+	
+	@RestController
+	@RequestMapping("/rooms")
+	public class RoomController {
+		@Autowired
+		private RoomRepository roomRepository;
+		
+		@GetMapping
+		public Iterable<Room> getRooms() {
+			return this.roomRepository.findAll();
+		}
+	}
+
+}
---
+++ src/main/java/com/marc/fors/learningspring/data/repository/RoomRepository.java
@@ -0,0 +1,11 @@
+package com.marc.fors.learningspring.data.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marc.fors.learningspring.data.entity.Room;
+
+@Repository
+public interface RoomRepository extends CrudRepository<Room, Long>{
+	
+}
